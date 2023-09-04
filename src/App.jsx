7aef5fa9b0359700166907602f77a9ec0ff0808e
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-09-04 23:03:08 +0330

    rendreing app using rrd

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import { Toaster } from "react-hot-toast";
 import Header from "./components/Header/Header";
 import LocationList from "./components/locationList/LocationList";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Toaster />
       <Header />
-      <LocationList />
+      <Routes>
+        <Route path="/" element={<LocationList />} />
+      </Routes>
     </div>
   );
 }
