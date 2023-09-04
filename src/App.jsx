a2548b6ca3412fe8c73cb82b861c6285f62b6758
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-09-04 22:03:37 +0330

    creating custom hook for fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Toaster } from "react-hot-toast";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
+      <Toaster />
       <Header />
     </div>
   );
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+
+export default function useFetch(url, query = "") {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${url}?${query}`);
+        setData(data);
+      } catch (error) {
+        setData([]);
+        toast.error(error?.msg);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, [query, url]);
+
+  return { isLoading, data };
+}
