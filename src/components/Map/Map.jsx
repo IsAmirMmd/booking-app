0d7c5cc923a839068303d486df9ba3b64623ad7c
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-09-05 13:01:52 +0330

    showing marker in map using react-leaflet

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,45 @@
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { useHotels } from "../context/HotelProvider";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Map = () => {
   const { isLoading, hotels } = useHotels();
+  const [mapCenter, setMapCenter] = useState([36.26664, 59.6436]);
+  const [searchParams, setSearchParmas] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
-  return <div className="mapContainer">map</div>;
+  useEffect(() => {
+    if (lat && lng) setMapCenter([lat, lng]);
+  }, [lat, lng]);
+
+  return (
+    <div className="mapContainer">
+      <MapContainer
+        className="map"
+        center={mapCenter}
+        zoom={10}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <ChangeCenter position={mapCenter} />
+        {hotels.map((item) => (
+          <Marker key={item.id} position={[item.latitude, item.longitude]}>
+            <Popup>{item.host_location}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Map;
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+}
